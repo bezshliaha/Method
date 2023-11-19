ccbfa6a64da2b948b20b6e93d3ffa6c86c3ce401
bezshliaha <bezshliaha@gmail.com>
2023-11-19 19:28:41 +0100

    Refactoring and type update

--- src/app/Main01.java
+++ src/app/Main01.java
@@ -3,6 +3,7 @@ package app;
 import java.util.Scanner;
 
 public class Main01 {
+
     private static final String CURRENCY = "EUR";
 
     public static void main(String[] args) {
@@ -11,28 +12,32 @@ public class Main01 {
         System.out.println("Enter the price in " + CURRENCY + ": ");
         double orderSum = Double.parseDouble(scanner.nextLine().replace(",", "."));
 
-        float discountPercent = calculateDiscountPercent((float) orderSum);
-        float orderSumToPayment = calculateOrderSumToPayment((float) orderSum, discountPercent);
-        float totalAmountEUR = (float) orderSum - orderSumToPayment;
+        double discountPercent = calculateDiscountPercent(orderSum);
+        double orderSumToPayment = calculateOrderSumToPayment(orderSum, discountPercent);
+        double totalAmountEUR = orderSum - orderSumToPayment;
 
-        displayOrderInfo((float) orderSum, orderSumToPayment, discountPercent, totalAmountEUR);
+        displayOrderInfo(orderSum, orderSumToPayment, discountPercent, totalAmountEUR);
         scanner.close();
     }
-    private static float calculateDiscountPercent(float orderSum) {
+
+    private static double calculateDiscountPercent(double orderSum) {
         if (orderSum <= 5000) {
-            return 5.0f;
+            return 5.0;
         } else if (orderSum <= 10000) {
-            return 10.0f;
+            return 10.0;
         } else {
-            return 15.0f;
+            return 15.0;
         }
     }
-    private static float calculateOrderSumToPayment(float orderSum, float discountPercent) {
+
+    private static double calculateOrderSumToPayment(double orderSum, double discountPercent) {
         return orderSum * (discountPercent / 100);
     }
-    private static void displayOrderInfo(float orderSum, float orderSumToPayment, float discountPercent, float totalAmountEUR) {
-        System.out.printf("Total price: %.2f %s%n", orderSum, CURRENCY);
-        System.out.printf("Discount amount: %.2f %s (%.2f%%)%n", orderSumToPayment, CURRENCY, discountPercent);
-        System.out.printf("Amount to pay: %.2f %s%n", totalAmountEUR, CURRENCY);
+
+    private static void displayOrderInfo(double orderSum, double orderSumToPayment,
+                                         double discountPercent, double totalAmountEUR)
+    {
+        System.out.printf("Total price: %.2f %s%nDiscount amount: %.2f %s (%.2f%%)%nAmount to pay: %.2f %s%n",
+                orderSum, CURRENCY, orderSumToPayment, CURRENCY, discountPercent, totalAmountEUR, CURRENCY);
     }
 }
